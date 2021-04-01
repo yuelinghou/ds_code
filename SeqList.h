a20b2cceb9b9063a9974cb5e19d2c310a9d9757c
yuelinghou <76507035+yuelinghou@users.noreply.github.com>
2021-04-01 22:25:16 +0800

    2021.4.1

--- SeqList.h
+++ SeqList.h
@@ -1,41 +1,30 @@
+// SeqList.h
 #pragma once
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
 
-//C库函数的头文件声明
-#include<stdio.h>
-#include<assert.h>
-#include<stdlib.h>
-
-//自定义顺序表元素的类型
-typedef int SeqListDateType;
-
-//顺序表的结构体
+typedef int SLDateType;
 typedef struct SeqList
 {
-	SeqListDateType* data;
+	SLDateType* a;
 	size_t size;
-	size_t capacity;
-}SL;
+	size_t capacity; // unsigned int
+}SeqList;
 
-//基本的增删查改接口
-//顺序表的初始化
-void SeqListInit(SL* ps);
-//顺序表销毁
-void SqeListDestory(SL* ps);
-//顺序表打印
-void SeqListPrint(SL* ps);
-//顺序表在pos位置插入x
-void SeqListInsert(SL* ps, size_t pos, SeqListDateType x);
-//顺序表删除pos位置的值
-void SeqListErase(SL* ps, size_t pos);
-//顺序表尾插
-void SeqListPushBack(SL* ps, SeqListDateType x);
-//顺序表尾删
-void SeqListPopBack(SL* ps);
-//顺序表头插
-void SeqListPushFront(SL* ps, SeqListDateType x);
-//顺序表头删
-void SeqListPopFront(SL* ps);
-//顺序表查找(找到了返回下标，找不到返回-1)
-int SeqListFind(SL* ps, SeqListDateType x);
+// 对数据的管理:增删查改 
+void SeqListInit(SeqList* ps);
+void SeqListDestory(SeqList* ps);
 
+void SeqListPrint(SeqList* ps);
+void SeqListPushBack(SeqList* ps, SLDateType x);
+void SeqListPushFront(SeqList* ps, SLDateType x);
+void SeqListPopFront(SeqList* ps);
+void SeqListPopBack(SeqList* ps);
 
+// 顺序表查找
+int SeqListFind(SeqList* ps, SLDateType x);
+// 顺序表在pos位置插入x
+void SeqListInsert(SeqList* ps, size_t pos, SLDateType x);
+// 顺序表删除pos位置的值
+void SeqListErase(SeqList* ps, size_t pos);
---
+++ test_2021_4_1.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//test.c
+#include<stdio.h>
+#include"SeqList.h"
+
+int main()
+{
+	//SeqList d;
+	//SeqListInit(&d);
+	///*SeqListPushBack(&d, 0);
+	//SeqListPushBack(&d, 1);
+	//SeqListPushBack(&d, 1);
+	//SeqListPushBack(&d, 1);*/
+	//SeqListPushFront(&d, 80);
+	//SeqListPushFront(&d, 70);
+	//SeqListErase(&d, 0);
+	//SeqListErase(&d, 0);
+	///*printf("%d\n", SeqListFind(&d, 7));*/
+	//SeqListPrint(&d);
+	//SeqListDestory(&d);
+	return 0;
+}
